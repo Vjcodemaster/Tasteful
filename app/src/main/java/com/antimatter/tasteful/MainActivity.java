805cb36b3acaf3d28446c247c0d7d7737f4f99aa
Vijay <vijayeh.android@gmail.com>
2019-01-08 21:32:47 +0530

    Change Log: **modified mainactivity, cornered button, MenuItemRvadapter **now data is being displayed dynamically from Mainactivity from string array xml **adding and substracting quantity is working fine **removed number_quantity xml from project **rv_menu_item xml is added for recyclerview management

--- app/src/main/java/com/antimatter/tasteful/MainActivity.java
+++ app/src/main/java/com/antimatter/tasteful/MainActivity.java
@@ -2,15 +2,35 @@ package com.antimatter.tasteful;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TableRow;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    public MenuItemRVAdapter menuItemRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.rv_menu_item_list);
 
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLinearLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
+        recyclerView.setHasFixedSize(true);
+
+        ArrayList<String> alFoodItems = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.food_items)));
+        menuItemRVAdapter = new MenuItemRVAdapter(MainActivity.this, recyclerView, alFoodItems);
+        recyclerView.setAdapter(menuItemRVAdapter);
         //TableRow trHeading = (TableRow) getLayoutInflater().inflate(R.layout.table_row_heading, null);
     }
 }
--- app/src/main/java/com/antimatter/tasteful/MenuItemRVAdapter.java
+++ app/src/main/java/com/antimatter/tasteful/MenuItemRVAdapter.java
@@ -6,11 +6,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MenuItemRVAdapter extends RecyclerView.Adapter<MenuItemRVAdapter.MenuItemTabHolder> {
 
+    Context context;
+    private RecyclerView recyclerView;
+    ArrayList<String> alFoodItems;
 
-    MenuItemRVAdapter(Context context, RecyclerView recyclerView) {
+    MenuItemRVAdapter(Context context, RecyclerView recyclerView, ArrayList alFoodItems) {
+        this.context = context;
+        this.recyclerView = recyclerView;
+        this.alFoodItems = alFoodItems;
     }
 
 
@@ -23,12 +34,40 @@ public class MenuItemRVAdapter extends RecyclerView.Adapter<MenuItemRVAdapter.Me
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MenuItemTabHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final MenuItemTabHolder holder, final int position) {
+        holder.tvMenuItem.setText(alFoodItems.get(position));
+        holder.tvSlNo.setText(String.valueOf(position+1));
+
+        holder.btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int n = Integer.valueOf(holder.tvQuantity.getText().toString());
+                if(n==10){
+                    Toast.makeText(context, "Maximum of 10 is allowed per item", Toast.LENGTH_SHORT).show();
+                } else {
+                    n++;
+                    holder.tvQuantity.setText(String.valueOf(n));
+                }
+            }
+        });
+
+        holder.btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int n = Integer.valueOf(holder.tvQuantity.getText().toString());
+                if(n==0){
+                    Toast.makeText(context, "Lets be positive :)", Toast.LENGTH_SHORT).show();
+                } else {
+                    n--;
+                    holder.tvQuantity.setText(String.valueOf(n));
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0; //alBeaconInfo.size()
+        return alFoodItems.size(); //alBeaconInfo.size()
     }
 
     @Override
@@ -43,9 +82,19 @@ public class MenuItemRVAdapter extends RecyclerView.Adapter<MenuItemRVAdapter.Me
         //Button btnEdit, btnRemove;
 
         //ImageView ivCircularDp;
+        TextView tvMenuItem;
+        TextView tvQuantity;
+        TextView tvSlNo;
+        Button btnPlus;
+        Button btnMinus;
 
         MenuItemTabHolder(View itemView) {
             super(itemView);
+            tvMenuItem = itemView.findViewById(R.id.tv_menu_item);
+            tvQuantity = itemView.findViewById(R.id.tv_quantity);
+            tvSlNo = itemView.findViewById(R.id.tv_sl_no);
+            btnPlus = itemView.findViewById(R.id.btn_plus);
+            btnMinus = itemView.findViewById(R.id.btn_minus);
             //civDP = itemView.findViewById(R.id.civ_dp);
         }
     }
